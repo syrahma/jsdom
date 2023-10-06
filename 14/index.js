5146406c238ba405c8aca456cd7c022c94d0dd9a
MD SYEDUR RAHMAN <syrahma@users.noreply.github.com>
2023-10-06 20:07:26 +0600

    Add files via upload

---
+++ 14/index.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector("#menu");
+menu.insertAdjacentHTML("beforebegin","<h1>Before begin</h1>");
+menu.insertAdjacentHTML("afterbegin","<h1>After begin</h1>");
+menu.insertAdjacentHTML("afterend","<h1>Afeter End</h1>");
+menu.insertAdjacentHTML("beforeend","<h1>Befor End</h1>");
