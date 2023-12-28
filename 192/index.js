a136fe463b63ad6382fd555ef5d124f15c6fdd6c
MD SYEDUR RAHMAN <syrahma@users.noreply.github.com>
2023-12-28 22:59:36 +0600

    Introduction to JavaScript insertAdjacentHTML() method
    Introduction to JavaScript insertAdjacentHTML() method

---
+++ 192/index.js
@@ -0,0 +1,5 @@
+let menu=document.querySelector("#menu");
+menu.insertAdjacentHTML("beforebegin","<h3>beforebegin</h3>");
+menu.insertAdjacentHTML("afterbegin","<h3>afterbegin</h3>");
+menu.insertAdjacentHTML("afterend","<h3>afterend</h3>");
+menu.insertAdjacentHTML("beforeend","<h3>beforeend</h3>");
