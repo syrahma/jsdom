5d4ebef31b5cbc21a2ef003ac54c4255db7538d8
MD SYEDUR RAHMAN <syrahma@users.noreply.github.com>
2023-12-29 22:35:46 +0600

    JavaScript Events
    JavaScript Events

---
+++ 200/index.js
@@ -0,0 +1,11 @@
+// let btn= document.querySelector("#btn");
+// btn.addEventListener("click",function(e){
+//  alert("Button was clicked");         
+
+// }
+
+// )
+
+function clicked(){
+  alert("The Button was clicked");        
+}
